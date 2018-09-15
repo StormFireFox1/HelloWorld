0f9094d1beb502ae0cc1d9ff0a21ead8a73472e2
StormFireFox1 <matei@gardus.eu>
2018-09-15 23:00:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# HelloWorld
+
+Welcome to the most useless repository ever.
+I like getting a feel for whatever language I think would work best for a specific scenario.
+This is a Git repository of elementary algorithm programs for every language I know so that I can see which one I like best.
+
+## Programming Languages List
+- C
---
+++ helloworld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// "Hello World"
+//
+// C was one of the first languages I have learned, and I still like to this day. It's the first language I wrote "Hello World" in.
+//
+// I don't use it as much anymore, but that's to be expected since I haven't really written anything higly-optimized in a long time.
+// That, and it reminds me of schoolwork, which always sucks. The manual memory managment is fun, especially when screwing up. Also,
+// this is the only language I've actually done point arithmetic in, which is insane when you think about it.
+//
+// This is the only "Hello World" I'll write on this project, since C is so close to my heart. The other languages get some awesome
+// elementary algorithms.
+int main()
+{
+    printf("Hello, World!")
+    return 0;
+}
